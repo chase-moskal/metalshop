ba2713a9438a5ef448914921c7b8554dca04b804
Chase Moskal <chasemoskal@gmail.com>
2018-12-13 17:57:29 -0800

    improve fsp readFile stuff

--- source/toolbox/fsp.ts
+++ source/toolbox/fsp.ts
@@ -1,11 +1,20 @@
 
 import * as fs from "fs"
 
-export async function readFile(path: string): Promise<Buffer> {
+export async function readFileBuffer(path: string): Promise<Buffer> {
 	return new Promise<Buffer>((resolve, reject) => {
 		fs.readFile(path, (error, data) => {
 			if (error) reject(error)
 			else resolve(data)
 		})
 	})
+}
+
+export async function readFile(path: string, encoding = "utf8"): Promise<string> {
+	const buffer = await readFileBuffer(path)
+	return buffer.toString(encoding)
+}
+
+export async function readJson<T extends Object = Object>(path: string): Promise<T> {
+	return JSON.parse(await readFile(path))
 }
