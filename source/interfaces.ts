e1faf4d16653fcd658c4829523727a142d4d6aea
Chase Moskal <chasemoskal@gmail.com>
2019-09-11 15:26:23 -0700

    interfaces: new microservices and some renames

--- source/interfaces.ts
+++ source/interfaces.ts
@@ -14,6 +14,22 @@ export interface AuthTokens {
 	refreshToken: RefreshToken
 }
 
+export interface Claims {
+	[key: string]: any
+}
+
+export interface User {
+	userId: number
+	claims: Claims
+}
+
+export interface Profile {
+	userId: string
+	picture: string
+	realname: string
+	nickname: string
+}
+
 export interface AuthExchangerTopic extends renraku.Topic<AuthExchangerTopic> {
 	authorize(options: {refreshToken: RefreshToken}): Promise<AccessToken>
 	authenticateViaGoogle(options: {googleToken: string}): Promise<AuthTokens>
@@ -29,11 +45,39 @@ export interface AccountPopupTopic extends renraku.Topic<AccountPopupTopic> {
 	login(): Promise<AuthTokens>
 }
 
+export interface ProfilerTopic extends renraku.Topic<ProfilerTopic> {
+	getProfile(options: {accessToken: AccessToken; userId: string}): Promise<Profile>
+	setProfile(options: {accessToken: AccessToken; profile: Profile}): Promise<void>
+}
+
+export interface ProfilerCacheTopic extends renraku.Topic<ProfilerCacheTopic> {
+	getProfile(options: {accessToken: AccessToken; userId: string}): Promise<Profile>
+	setProfile(options: {accessToken: AccessToken; profile: Profile}): Promise<void>
+}
+
+export interface ClaimsVanguardTopic extends renraku.Topic<ClaimsVanguardTopic> {
+	createUser(options: {googleId: User}): Promise<User>
+	getUser(options: {userId: string}): Promise<User>
+	setClaims(options: {userId: string; claims: Claims}): Promise<User>
+}
+
+export interface PaywallGuardianTopic extends renraku.Topic<PaywallGuardianTopic> {
+	makeUserPremium({accessToken: AccessToken}): Promise<AccessToken>
+	revokeUserPremium({accessToken: AccessToken}): Promise<AccessToken>
+}
+
 export interface AuthExchangerApi extends renraku.Api<AuthExchangerApi> {
 	authExchanger: AuthExchangerTopic
 }
 
-export interface User {
-	userId: number
-	claims: {[key: string]: any}
+export interface ProfilerApi extends renraku.Api<ProfilerApi> {
+	profiler: ProfilerTopic
+}
+
+export interface ClaimsVanguardApi extends renraku.Api<ClaimsVanguardApi> {
+	claimsVanguard: ClaimsVanguardTopic
+}
+
+export interface PaywallGuardianApi extends renraku.Api<PaywallGuardianApi> {
+	paywallGuardian: PaywallGuardianTopic
 }
--- source/shapes.ts
+++ source/shapes.ts
@@ -1,10 +1,38 @@
 
 import * as renraku from "renraku"
-import {AuthExchangerApi} from "./interfaces.js"
+
+import {
+	ProfilerApi,
+	AuthExchangerApi,
+	ClaimsVanguardApi,
+	PaywallGuardianApi,
+} from "./interfaces.js"
 
 export const authExchangerApiShape: renraku.ApiShape<AuthExchangerApi> = {
 	authExchanger: {
 		authorize: true,
-		authenticateViaGoogle: true
+		authenticateViaGoogle: true,
+	}
+}
+
+export const profilerApiShape: renraku.ApiShape<ProfilerApi> = {
+	profiler: {
+		getProfile: true,
+		setProfile: true,
+	}
+}
+
+export const claimsVanguardApiShape: renraku.ApiShape<ClaimsVanguardApi> = {
+	claimsVanguard: {
+		createUser: true,
+		getUser: true,
+		setClaims: true,
+	}
+}
+
+export const paywallGuardianApiShape: renraku.ApiShape<PaywallGuardianApi> = {
+	paywallGuardian: {
+		makeUserPremium: true,
+		revokeUserPremium: true,
 	}
 }
