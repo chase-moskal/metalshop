65a5f915ddfdd8193e55142444b290b77ff98d12
Chase Moskal <chasemoskal@gmail.com>
2020-02-20 16:05:40 -0800

    fix: make profile magistrate client async

--- source/clients/create-profile-magistrate-client.ts
+++ source/clients/create-profile-magistrate-client.ts
@@ -4,8 +4,8 @@ import {apiClient} from "renraku/dist/api-client.js"
 import {profileShape} from "../shapes.js"
 import {ProfileApi} from "../interfaces.js"
 
-export function createProfileMagistrateClient({url}: {url: string}) {
-	const {profileMagistrate} = apiClient<ProfileApi>({
+export async function createProfileMagistrateClient({url}: {url: string}) {
+	const {profileMagistrate} = await apiClient<ProfileApi>({
 		url,
 		shape: profileShape
 	})
