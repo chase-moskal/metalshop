954b38256e0aa9da9b968a4bc6d012248970a5b0
Chase Moskal <chasemoskal@gmail.com>
2020-04-04 14:28:49 -0700

    add: mock options

--- source/business/auth-api/mock-verify-google-token.ts
+++ source/business/auth-api/mock-verify-google-token.ts
@@ -1,11 +1,13 @@
 
 import {GoogleResult, VerifyGoogleToken} from "./interfaces.js"
 
-export function mockVerifyGoogleToken(): VerifyGoogleToken {
+export function mockVerifyGoogleToken({googleResult}: {
+	googleResult?: GoogleResult
+} = {}): VerifyGoogleToken {
 	let count = 0
-	return async(googleToken: string): Promise<GoogleResult> => ({
+	return async(googleToken: string): Promise<GoogleResult> => googleResult || {
 		googleId: `g123456-${count}`,
 		name: `Faker McFakerson ${++count}`,
 		avatar: "https://picsum.photos/id/375/200/200",
-	})
+	}
 }
