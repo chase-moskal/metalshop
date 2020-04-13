5674787ceca6ce081789f0425bc8af7e6e155871
Chase Moskal <chasemoskal@gmail.com>
2020-04-12 22:05:41 -0700

    logger: refactor

--- source/toolbox/logger.ts
+++ source/toolbox/logger.ts
@@ -3,65 +3,62 @@ import chalk from "../commonjs/chalk.js"
 import {Chalk} from "../commonjs/chalk.js"
 
 export interface SimpleConsole {
-	log(...args: any[]): void
-	info(...args: any[]): void
-	debug(...args: any[]): void
-	warn(...args: any[]): void
-	error(...args: any[]): void
-	clear(): void
+	log: typeof console.log
+	info: typeof console.info
+	debug: typeof console.debug
+	warn: typeof console.warn
+	error: typeof console.error
+	clear: typeof console.clear
 }
 
 export class Logger implements SimpleConsole {
-	private _chalk: Chalk
-	private _console: SimpleConsole
-	private get _timestamp() {
-		const date = new Date()
-		const year = date.getUTCFullYear()
-		const month = (date.getUTCDate() + 1).toString().padStart(2, "0")
-		const day = date.getUTCDate().toString().padStart(2, "0")
-		const hours = date.getUTCHours().toString().padStart(2, "0")
-		const minutes = date.getUTCMinutes().toString().padStart(2, "0")
-		const seconds = date.getUTCSeconds().toString().padStart(2, "0")
-		const milliseconds = date.getUTCMilliseconds().toString().padStart(3, "0")
-		const calendar = `${year}-${month}-${day}`
-		const clock = `${hours}:${minutes}:${seconds}.${milliseconds}`
-		return `[${calendar}:${clock}]`
-	}
+	log: typeof console.log
+	info: typeof console.info
+	debug: typeof console.debug
+	warn: typeof console.warn
+	error: typeof console.error
+	clear: typeof console.clear
 
-	constructor({_console = console, force = true}: {
-		force?: boolean
-		_console?: Console
+	constructor({con = console, colors = true}: {
+		con?: Console
+		colors?: boolean
 	} = {}) {
-		this._console = _console
-		this._chalk = new chalk.Instance(force ? {level: 1}: undefined)
-	}
+		let chalkstick: Chalk
+		if (colors === true)
+			chalkstick = new chalk.Instance({level: 1})
+		else if (colors === false)
+			chalkstick = new chalk.Instance({level: 0})
+		else
+			chalkstick = new chalk.Instance()
 
-	log(...args: any[]): void {
-		const {_console, _chalk, _timestamp} = this
-		_console.log(_chalk.grey(_timestamp), ...args)
-	}
+		const timestamp = () => {
+			const date = new Date()
+			const year = date.getUTCFullYear()
+			const month = (date.getUTCDate() + 1).toString().padStart(2, "0")
+			const day = date.getUTCDate().toString().padStart(2, "0")
+			const hours = date.getUTCHours().toString().padStart(2, "0")
+			const minutes = date.getUTCMinutes().toString().padStart(2, "0")
+			const seconds = date.getUTCSeconds().toString().padStart(2, "0")
+			const milliseconds = date.getUTCMilliseconds().toString().padStart(3, "0")
+			const calendar = `${year}-${month}-${day}`
+			const clock = `${hours}:${minutes}:${seconds}.${milliseconds}`
+			return `[${calendar} ${clock}]`
+		}
 
-	info(...args: any[]): void {
-		const {_console, _chalk, _timestamp} = this
-		_console.info(_chalk.cyanBright(_timestamp), ...args)
-	}
+		const prepare = (
+			logfunc: (...args: any[]) => void,
+			colorfunc: (s: string) => string
+		) => (...args: any[]) => logfunc.call(
+			con,
+			colorfunc.call(chalkstick, timestamp()),
+			...args
+		)
 
-	debug(...args: any[]): void {
-		const {_console, _chalk, _timestamp} = this
-		_console.debug(_chalk.cyan(_timestamp), ...args)
-	}
-
-	warn(...args: any[]): void {
-		const {_console, _chalk, _timestamp} = this
-		_console.warn(_chalk.yellow(_timestamp), ...args)
-	}
-
-	error(...args: any[]): void {
-		const {_console, _chalk, _timestamp} = this
-		_console.error(_chalk.red(_timestamp), ...args)
-	}
-
-	clear(): void {
-		this._console.clear()
+		this.log = prepare(con.log, chalkstick.cyan)
+		this.info = prepare(con.info, chalkstick.cyanBright)
+		this.debug = prepare(con.debug, chalkstick.blueBright)
+		this.warn = prepare(con.warn, chalkstick.yellow)
+		this.error = prepare(con.error, chalkstick.red)
+		this.clear = () => con.clear()
 	}
 }
