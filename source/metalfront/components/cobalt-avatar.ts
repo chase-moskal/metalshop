6be6bea314a7d46b12e3b4fabb5698d970b0f596
Chase Moskal <chasemoskal@gmail.com>
2020-07-30 22:48:40 -0700

    improve: cobalt avatar, badges and rounded

--- source/metalfront/components/cobalt-avatar.ts
+++ source/metalfront/components/cobalt-avatar.ts
@@ -1,15 +1,60 @@
 
+import {star} from "../system/icons.js"
 import {MetalUser} from "../../types.js"
 import {silhouette} from "../system/icons.js"
 import {mixinStyles} from "../framework/mixin-styles.js"
 import {MetalshopComponent, html, property, css, TemplateResult} from "../framework/metalshop-component.js"
 
+import * as evaluators from "../../business/core/user-evaluators.js"
+
 const styles = css`
+
+:host {
+	--internal-badge-size: calc(var(--cobalt-avatar-size, 6em) / 4);
+}
+
+.container {
+	position: relative;
+}
+
+.avatar {
+	width: var(--cobalt-avatar-size, 6em);
+	height: var(--cobalt-avatar-size, 6em);
+}
+
+:host([rounded]) .avatar {
+	overflow: hidden;
+	border-radius: var(--cobalt-avatar-size, 6em);
+}
 
 img, svg {
 	display: block;
-	width: var(--cobalt-avatar-size, 6em);
-	height: var(--cobalt-avatar-size, 6em);
+	width: 100%;
+	height: 100%;
+}
+
+.badges {
+	position: absolute;
+	top: -10%;
+	left: -10%;
+	display: flex;
+	flex-wrap: wrap;
+	width: 120%;
+}
+
+.badges > span {
+	position: relative;
+	width: var(--internal-badge-size);
+	height: var(--internal-badge-size);
+	border-radius: var(--internal-badge-size);
+	color: yellow;
+	background: #7a7a008a;
+}
+
+.badges svg {
+	position: absolute;
+	width: 100%;
+	height: 100%;
 }
 
 `
@@ -25,11 +70,30 @@ export class CobaltAvatar extends MetalshopComponent<void> {
 
 	render() {
 		const {user, fallbackAvatar} = this
-		const avatar = user?.profile?.avatar
+		const avatar = user?.profile.avatar
 		return html`
-			${avatar
-				? html`<img src=${avatar} alt="[avatar]"/>`
-				: fallbackAvatar}
+			<div class=container>
+				<div class=avatar>
+					${avatar
+						? html`<img src=${avatar} alt="[avatar]"/>`
+						: fallbackAvatar}
+				</div>
+				${this.renderBadges(user)}
+			</div>
+		`
+	}
+
+	private renderBadges(user?: MetalUser) {
+		if (!user) return null
+
+		const premiumBadge = evaluators.isPremium(user) ? html`
+			<span title="premium member">${star}</span>
+		` : null
+
+		return html`
+			<div class=badges>
+				${premiumBadge}
+			</div>
 		`
 	}
 }
