47c0ad86e38fb06616cbab52dd224e9777b057f5
Chase Moskal <chasemoskal@gmail.com>
2020-08-03 23:45:02 -0700

    fix: start open mock

--- source/metalfront/start-metalshop-demo.ts
+++ source/metalfront/start-metalshop-demo.ts
@@ -103,6 +103,10 @@ function randomAvatar() {
 		"https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=face&cs=tinysrgb&w=200&h=200&fit=crop&ixid=eyJhcHBfaWQiOjF9",
 		"https://images.unsplash.com/photo-1528892952291-009c663ce843?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&h=200&fit=crop&ixid=eyJhcHBfaWQiOjF9",
 		"https://images.unsplash.com/photo-1493106819501-66d381c466f1?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&h=200&fit=crop&ixid=eyJhcHBfaWQiOjF9",
+		"https://images.unsplash.com/photo-1542909168-82c3e7fdca5c?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&h=200&fit=crop&ixid=eyJhcHBfaWQiOjF9",
+		"https://images.unsplash.com/flagged/photo-1554176778-0319e4eb4516?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&h=200&fit=crop&ixid=eyJhcHBfaWQiOjF9",
+		"https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&h=200&fit=crop&ixid=eyJhcHBfaWQiOjF9",
+
 		
 		// women
 		"https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&h=200&fit=crop&ixid=eyJhcHBfaWQiOjF9",
@@ -112,6 +116,13 @@ function randomAvatar() {
 		"https://images.unsplash.com/photo-1554151228-14d9def656e4?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&h=200&fit=crop&ixid=eyJhcHBfaWQiOjF9",
 		"https://images.unsplash.com/photo-1526080652727-5b77f74eacd2?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&h=200&fit=crop&ixid=eyJhcHBfaWQiOjF9",
 		"https://images.unsplash.com/photo-1496203695688-3b8985780d6a?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&h=200&fit=crop&ixid=eyJhcHBfaWQiOjF9",
+		"https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&h=200&fit=crop&ixid=eyJhcHBfaWQiOjF9",
+		"https://images.unsplash.com/photo-1551843073-4a9a5b6fcd5f?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&h=200&fit=crop&ixid=eyJhcHBfaWQiOjF9",
+		"https://images.unsplash.com/photo-1528809217021-151305b50e55?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&h=200&fit=crop&ixid=eyJhcHBfaWQiOjF9",
+		"https://images.unsplash.com/photo-1560762354-1be2f3d3c3f6?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&h=200&fit=crop&ixid=eyJhcHBfaWQiOjF9",
+		"https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&h=200&fit=crop&ixid=eyJhcHBfaWQiOjF9",
+		"https://images.unsplash.com/photo-1487546511569-62a31e1c607c?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&h=200&fit=crop&ixid=eyJhcHBfaWQiOjF9",
+		"https://images.unsplash.com/photo-1531251445707-1f000e1e87d0?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&h=200&fit=crop&ixid=eyJhcHBfaWQiOjF9",
 	]
 	return randomSample(Math.random(), urls)
 }
@@ -148,6 +159,7 @@ function randomAvatar() {
 	const day = minute * 60 * 24
 
 	const {mock = ""} = parseQuery<{mock: string}>()
+	const startOpen = mock.includes("open")
 	const startAdmin = mock.includes("admin")
 	const startStaff = mock.includes("staff")
 	const startBanned = mock.includes("banned")
@@ -246,6 +258,11 @@ function randomAvatar() {
 		...components,
 		...demoComponents,
 	})
+
+	if (startOpen) {
+		const menuDisplay: any = document.querySelector("menu-display[data-label=account]")
+		menuDisplay.toggle()
+	}
 
 	// start
 	await supermodel.auth.useExistingLogin()
