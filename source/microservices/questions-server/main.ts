777e38cff7b9fb4b0ac63528327cbe146fed5f2d
Chase Moskal <chasemoskal@gmail.com>
2020-08-12 15:53:05 -0700

    tweak: questions server imports

--- source/microservices/questions-server/main.ts
+++ source/microservices/questions-server/main.ts
@@ -13,7 +13,7 @@ import {connectMongo} from "../../toolbox/connect-mongo.js"
 import {unpackCorsConfig} from "../../toolbox/unpack-cors-config.js"
 
 import * as evaluators from "../../business/core/user-evaluators.js"
-import { makeCoreSystemsClients } from "../../business/core/core-clients.js"
+import {makeCoreSystemsClients} from "../../business/core/core-clients.js"
 import {makeQuestionQuarry} from "../../business/questions/question-quarry.js"
 
 import {QuestionsServerConfig, QuestionRow, QuestionLikeRow, QuestionReportRow, QuestionsApi, MetalUser} from "../../types.js"
