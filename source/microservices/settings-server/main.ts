2193b26da9411f3fdf3e2fb4f4f94e09748c22e3
Chase Moskal <chasemoskal@gmail.com>
2020-08-12 16:07:50 -0700

    add: settings server

---
+++ source/microservices/settings-server/main.ts
@@ -0,0 +1,56 @@
+
+import {apiServer} from "renraku/dist/api-server.js"
+import {curryVerifyToken} from "redcrypto/dist/curries/curry-verify-token.js"
+
+import Koa from "../../commonjs/koa.js"
+import mount from "../../commonjs/koa-mount.js"
+
+import {health} from "../../toolbox/health.js"
+import {read, readYaml} from "../../toolbox/reading.js"
+import {nodeProgram} from "../../toolbox/node-program.js"
+import {connectMongo} from "../../toolbox/connect-mongo.js"
+import {unpackCorsConfig} from "../../toolbox/unpack-cors-config.js"
+
+import {SettingsServerConfig, SettingsApi, SettingsRow} from "../../types.js"
+import {makeSettingsSheriff} from "../../business/settings/settings-sheriff.js"
+
+nodeProgram(async function main({logger}) {
+	const paths = {
+		config: "metalback/config/config.yaml",
+		authServerPublicKey: "metalback/config/auth-server.public.pem",
+	}
+
+	const config: SettingsServerConfig = await readYaml(paths.config)
+	const {debug, authServerOrigin} = config
+	const {port} = config.settingsServer
+	const cors = unpackCorsConfig(config.cors)
+
+	const authServerPublicKey = await read(paths.authServerPublicKey)
+	const verifyToken = curryVerifyToken(authServerPublicKey)
+
+	const {dbbyTable} = await connectMongo(config.mongo)
+	const settingsTable = dbbyTable<SettingsRow>("settings")
+
+	const settingsSheriff = makeSettingsSheriff({
+		settingsTable,
+		authorize: verifyToken,
+	})
+
+	const {koa: apiKoa} = await apiServer<SettingsApi>({
+		debug,
+		logger,
+		exposures: {
+			settingsSheriff: {
+				cors,
+				exposed: settingsSheriff,
+			},
+		}
+	})
+	
+	new Koa()
+		.use(health({logger}))
+		.use(mount("/api", apiKoa))
+		.listen({host: "0.0.0.0", port})
+	
+	logger.info(`🌐 settings-server on ${port}`)
+})
