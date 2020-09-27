cdc963bb6f5f676d034a46433a8ac0b7bf7de646
Chase Moskal <chasemoskal@gmail.com>
2020-09-26 19:01:58 -0700

    tidy: cleanup dbby

--- source/toolbox/dbby/dbby-helpers.ts
+++ source/toolbox/dbby/dbby-helpers.ts
@@ -1,17 +1,5 @@
 
-import {DbbyConditionTree, DbbyCondition, DbbyRow, DbbyConditionHelper} from "./dbby-types.js"
-
-// export function and<Row extends DbbyRow>(
-// 		...conditions: DbbyCondition<Row>[]
-// 	): DbbyConditionTree<Row> {
-// 	return ["and", ...conditions]
-// }
-
-// export function or<Row extends DbbyRow>(
-// 		...conditions: DbbyCondition<Row>[]
-// 	): ["or", ...DbbyCondition<Row>[]] {
-// 	return ["or", ...conditions]
-// }
+import {DbbyRow, DbbyConditionHelper} from "./dbby-types.js"
 
 export function curryDbbyHelpers<Row extends DbbyRow>(): {
 		and: DbbyConditionHelper<Row>
@@ -22,33 +10,3 @@ export function curryDbbyHelpers<Row extends DbbyRow>(): {
 		or: (...conditions) => ["or", ...conditions],
 	}
 }
-
-// export type CondOp<Row extends DbbyRow> = (...conditions: DbbyCondition<Row>[]) => DbbyConditionTree<Row>
-
-// export function cond<Row extends DbbyRow>(
-// 		builder: (o: {and: CondOp<Row>, or: CondOp<Row>}) => DbbyConditionTree<Row>
-// 	) {
-// 	return {
-// 		conditions: builder({and, or})
-// 	}
-// }
-
-// export const cond2 = Object.freeze({
-// 	and: <Row extends DbbyRow>(...conds: DbbyCondition<Row>[]) => ({
-// 		conditions: and(...conds)
-// 	}),
-// 	or: <Row extends DbbyRow>(...conds: DbbyCondition<Row>[]) => ({
-// 		conditions: or(...conds)
-// 	}),
-// })
-
-// export function find<Row extends DbbyRow>() {
-// 	return {
-// 		and: (...conds: DbbyCondition<Row>[]) => ({
-// 			conditions: and(...conds)
-// 		}),
-// 		or: (...conds: DbbyCondition<Row>[]) => ({
-// 			conditions: or(...conds)
-// 		}),
-// 	}
-// }
